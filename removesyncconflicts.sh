9d94cbfc05b9c33b36ecf0ce8798e664c5fd9479
Merlin <merlin@pop-os.localdomain>
2019-09-12 08:08:20 +0900

    This works now.

--- removesyncconflicts.sh
+++ removesyncconflicts.sh
@@ -1,45 +1,22 @@
 #!/bin/bash
-
-strindex() { 
-  x="${1%%$2*}"
-  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
-}
-
 find . -iname "*.sync-conflict-*" -print0 | while IFS= read -r -d '' scfile; do
+  echo Found $scfile
+  ORIGINALFILENAME=$(echo $scfile | sed -E "s/.sync-conflict-[0-9]+-[0-9]+-[0-9A-Z]+//g")
 
-    echo Found $scfile
+  if [ ! -f "$ORIGINALFILENAME" ]; then
+    echo Error: Conflict file $scfile exists but original file $ORIGINALFILENAME not found.
+    continue
+  fi
 
-    # FILESIZE=$(stat -c%s "$scfile")
-    CONFLICTHASH=$(sha256sum "$scfile" | cut -d " " -f 1 )
+  ORIGINALHASH=$(sha256sum "$ORIGINALFILENAME" | cut -d " " -f 1)
+  CONFLICTHASH=$(sha256sum "$scfile" | cut -d " " -f 1)
 
-    echo $scfile hash is $CONFLICTHASH
-
-    ORIGINALFILENAME=$(sed /s/sync-conflict.\./)
-
-    FILENAMEHEAD=${scfile#*.sync-conflict}
-    FILENAMETAIL=${scfile%}
-    # ORIGINALNAME=${scfile::-38}
-    extension="${scfile##*.}"
-    echo Original name is $ORIGINALNAME
-
-    BASENAME=$ORIGINALNAME"."$extension
-    # ORIGINALHASH=$(sha256sum "$BASENAME")
-
-    # # BASENAMESIZE=$(stat -c%s "$BASENAME")
-    # # echo "SYNC CONF SIZE +$FILESIZE "
-    # # echo "BASNAME FILE S +$BASENAMESIZE"
-    # # if [ "$FILESIZE" ]; then
-
-    #     # if [ $FILESIZE -gt $BASENAMESIZE ]; then
-    #     if [ "$CONFLICTHASH" = "$ORIGINALHASH" ]; then
-    #         echo "deleting original"
-    #         echo "$BASENAME"
-    #         echo mv "$BASENAME" /media/large_disk/Docs_notbackup/tobedeleted
-    #         #move syncconflict over original
-    #         echo mv "$scfile" "$BASENAME"
-    #     else
-    #         echo "deleting syncconfig file"
-    #         echo mv "$scfile" /media/large_disk/Docs_notbackupt/tobedeleted
-    #     fi
-    # # fi
+  if [ $ORIGINALHASH = $CONFLICTHASH ]; then
+    echo Deleting $scfile with hash identical to $ORIGINALFILENAME
+    rm "$scfile"
+  else
+    echo Files $ORIGINALFILENAME and $scfile found but hashes are different:
+    echo $ORIGINALHASH
+    echo $CONFLICTHASH
+  fi
 done
