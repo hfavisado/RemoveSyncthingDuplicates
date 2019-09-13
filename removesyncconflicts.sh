ea0fe66ebc1f9403d29fc085b4a7d4d936bda837
Merlin <merlin@pop-os.localdomain>
2019-09-13 10:17:02 +0900

    Put hash step in a function Made messages clearer

--- removesyncconflicts.sh
+++ removesyncconflicts.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
+
+gethash() {
+  echo $(sha256sum "$1" | cut -d " " -f 1)
+}
+
 find . -iname "*.sync-conflict-*" -print0 | while IFS= read -r -d '' scfile; do
   echo Found $scfile
   ORIGINALFILENAME=$(echo $scfile | sed -E "s/.sync-conflict-[0-9]+-[0-9]+-[0-9A-Z]+//g")
 
   if [ ! -f "$ORIGINALFILENAME" ]; then
-    echo Error: Conflict file $scfile exists but original file $ORIGINALFILENAME not found.
+    echo Error: Duplicate file $scfile exists but original file $ORIGINALFILENAME was not found.
     continue
   fi
 
-  ORIGINALHASH=$(sha256sum "$ORIGINALFILENAME" | cut -d " " -f 1)
-  CONFLICTHASH=$(sha256sum "$scfile" | cut -d " " -f 1)
+  ORIGINALHASH=$(gethash "$ORIGINALFILENAME")
+  CONFLICTHASH=$(gethash "$scfile")
 
   if [ $ORIGINALHASH = $CONFLICTHASH ]; then
     echo Deleting $scfile with hash identical to $ORIGINALFILENAME
     rm "$scfile"
   else
-    echo Files $ORIGINALFILENAME and $scfile found but hashes are different:
-    echo $ORIGINALHASH
-    echo $CONFLICTHASH
+    echo Error: Files $ORIGINALFILENAME and $scfile found but hashes are different:
+    echo original: $ORIGINALHASH
+    echo duplicate: $CONFLICTHASH
   fi
 done
